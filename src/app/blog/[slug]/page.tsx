46d03ab5e04f6b5f89702aef24b318df29fbcd42
furkanavc1 <furkanavci039@gmail.com>
2025-09-16 09:49:19 +0300

    raw folder structure, pages

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blogpost = () => {
+  return <div>post</div>;
+};
+
+export default Blogpost;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  // fetch("https://dummyjson.com/c/a7c4-016a-47aa-8241").then((res) =>
+  //   res.json().then((data) => console.log(data))
+  // );
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
